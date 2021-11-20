f65df8880e89cb203b5ac6a40e6ce70051b828f6
Column01 <colinandress999@gmail.com>
2021-11-20 18:12:10 -0500

    Comment event and manager stuff, typehint change

--- events/event.py
+++ events/event.py
@@ -1,4 +1,4 @@
-from typing import Type, Callable
+from typing import Coroutine, Type
 
 from services.service import Service
 
@@ -6,34 +6,60 @@ from services.service import Service
 class Event:
     """ Base Event Class """
     async def cancel(self):
+        """ Cancels the event """
         self.cancelled = True
     
     async def is_cancelled(self):
+        """ Gets if the event was cancelled """
         return self.cancelled
 
+# Typing stuff
 EventClass = Type(Event)
 ServiceClass = Type(Service)
+
 
 class EventRegistry:
     """ Event Registry Class, used for registering and dispatching all events """
     def __init__(self):
         self.event_listeners = {}
 
-    async def register(self, event: EventClass, service: ServiceClass, callback: Callable):
+    def register(self, event: EventClass, service: ServiceClass, callback: Coroutine):
+        """Register a coroutine to handle an event
+
+        Args:
+            event (EventClass): The event you want to handle
+            service (ServiceClass): An instance of the plugin that's handling it
+            callback (Coroutine): The coroutine that will be used to handle the event
+        """
         if self.event_listeners.get(event) is None:
             self.event_listeners[event] = []
         self.event_listeners[event].append((service, callback))
 
-    async def unregister(self, event: EventClass, service: ServiceClass, callback: Callable):
+    def unregister(self, event: EventClass, service: ServiceClass, callback: Coroutine):
+        """Un-registers the coroutine for handling the event
+
+        Args:
+            event (EventClass): The event for the listener you want to unregister
+            service (ServiceClass): An instance of the plugin that was handling it
+            callback (Coroutine): The coroutine that used to handle the event
+        """
         if self.event_listeners.get(event) is not None:
             self.event_listeners[event].remove((service, callback))
 
     async def dispatch(self, event: EventClass):
+        """Dispatches an event
+
+        Args:
+            event (EventClass): An instance of the event you want to dispatch
+        """
+        # Copy the event listeners to avoid mutation
+        _event_listeners = self.event_listeners.copy()
         # Get all listeners for that event
-        listeners = self.event_listeners.get(event)
+        listeners = _event_listeners.get(event)
         if listeners is not None:
             # Dispatch the event to the callback
             for _, callback in listeners:
                 await callback(event)
+                # If the callback cancels the event, stop dispatching
                 if await event.is_cancelled():
                     break
--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ import os
 
 
 class Manager:
+    """ The manager class that handles starting services and servers """
     def __init__(self, shared_path: str, server_name: str, server_path: str):
         self.shared_path = shared_path
         self.server_name = server_name
@@ -13,6 +14,7 @@ class Manager:
         self.log_path = os.path.join(self.shared_path, f"{self.server_name}.txt")
     
     def start(self):
+        """ Initializes services and then starts the server """
         with open(self.log_path, "w+") as fp:
             fp.write("Process forked and started the manager successfully!\n")
             fp.write(f"Socket path: {self.sock_path}, PID: {os.getpid()}\n")
