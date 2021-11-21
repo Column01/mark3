ab58de174821c2f56e5827b903cbcf8c4cf8d897
Column01 <colinandress999@gmail.com>
2021-11-20 19:47:52 -0500

    Fix manager async start, add cmd code comments
    Fixed the manager start code to run under asyncio
    Added some comments for start and base command

--- commands.py
+++ commands.py
@@ -4,6 +4,7 @@ import asyncio
 from manager import Manager
 
 class BaseCommand:
+    """ Base command class """
     def __init__(self):
         pass
 
@@ -12,29 +13,42 @@ class BaseCommand:
 
 
 class StartCommand(BaseCommand):
+    """ Command to start a server """
     def execute(self, server_path):
+        # Get the server path
         self.server_path = server_path
         self.shared_path = "test/"
+        # Verify it's actually a directory
         valid_path = self.verify_server_path()
         if valid_path:
+            # Daemonize the process
             if self.daemonize():
+                # Get the PID of the forked process and save it to a file
                 with open(os.path.join(self.shared_path, f"{self.server_name}.pid"), 'w') as f:
                     f.write(f"{os.getpid()}\n")
+                # Start an asyncio event loop
                 loop = asyncio.get_event_loop()
+                # Create an instance of the manager
                 manager = Manager(self.shared_path, self.server_name, self.server_path)
-                manager.start()
                 _ = loop.call_soon(manager.start)
-                sys.exit(0)
+                
+                # Blocking call interrupted by loop.stop()
+                try:
+                    loop.run_forever()
+                finally:
+                    loop.close()
+                    sys.exit(0)
             sys.exit(0)
         else:
             exit(f"Invalid server path: {server_path}")
 
     def daemonize(self):
+        """ Daemonizes the process to run in the background """
         if os.fork() > 0:
             # Parent process, we just wanna exit
             return False
 
-        # Child process, time to do stuff :D
+        # Child process, time to do stuff
         os.chdir(".")
         os.setsid()
         null = os.open(os.devnull, os.O_RDWR)
