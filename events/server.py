706882323267d8ab9a55d318e2e9aa51e2e0b1d6
Column01 <colinandress999@gmail.com>
2021-11-23 14:09:18 -0500

    Add some more server events

--- events/server.py
+++ events/server.py
@@ -7,3 +7,17 @@ class ServerStart(Event):
         self.server_name = server_name
         self.server_path = server_path
         super().__init__()
+
+
+class ServerOutput(Event):
+    """ Issued when the server process issues a line to STDOUT """
+    def __init__(self, line: str):
+        self.line = line
+        super().__init__()
+
+
+class ServerStopped(Event):
+    """ Issued when the server process stops """
+    def __init__(self, reason: str):
+        self.reason = reason
+        super().__init__()
