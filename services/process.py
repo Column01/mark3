de0c61323ef794b9a865b4f6266154dbf9308dd3
Column01 <colinandress999@gmail.com>
2021-11-23 14:10:13 -0500

    Code to start a test process and read its output

--- services/process.py
+++ services/process.py
@@ -1,41 +1,97 @@
+import asyncio
 import collections
 import logging
+import typing
 from asyncio import SubprocessProtocol, transports
+from asyncio.events import AbstractEventLoop
 
-from events.server import ServerStart
+from events.server import ServerOutput, ServerStart, ServerStopped
 from plugins.plugin import Plugin
+
+if typing.TYPE_CHECKING:
+    from events.event import EventRegistry
 
 
 class ProcessProtocol(SubprocessProtocol):
     """ Process Protocol that handles server process I/O """
-    def __init__(self, exit_future):
-        # Exit callback
+    def __init__(self, exit_future: typing.Coroutine, event_registry: "EventRegistry", loop: AbstractEventLoop):
+        # Exit future
         self.exit_future = exit_future
-        # Scrollback buffer
+
+        # Running event loop
+        self.loop = loop
+
+        # Reference to the event registry
+        self.event_registry = event_registry
+
         self.scrollback_buffer = collections.deque(maxlen=1000)
 
     def connection_made(self, transport: transports.BaseTransport):
-        print("Connection with process established")
+        logging.info("Connection with process established")
         self.transport = transport
+
+        self.stdin = self.transport.get_pipe_transport(0)
+        self.stdout = self.transport.get_pipe_transport(1)
+        self.stderr = self.transport.get_pipe_transport(2)
+        logging.info("Saved references to STDIN/OUT/ERR")
 
     def pipe_data_received(self, fd: int, data: bytes):
         # Decode the data from the process and store it
-        message = data.decode()
-        self.scrollback_buffer.append(message)
-        print(f"Data received: {message}")
+        message = data.decode().strip()
+
+        # Check if the output was on the server STDOUT
+        if fd == 1:
+            # Dispatch the server output event to the event bus
+            self.loop.create_task(self.event_registry.dispatch(ServerOutput(message)))
 
     def process_exited(self):
-        self.exit_future.set_result(True)
+        code = self.transport.get_returncode()
+        self.loop.create_task(self.exit_future(code))
 
-    def write_process(self, data: bytes):
-        self.transport.write(data)
+    async def write_process(self, data: typing.Union[bytes, str]):
+        """Writes data to the STDIN of the process
+
+        Args:
+            data (typing.Union[bytes, str]): The bytes or string to send to the process
+        """
+        if isinstance(data, str):
+            data = data.encode()
+        self.stdin.write(data)
 
 
 class Process(Plugin):
     """ The plugin that starts the server process """
     async def setup(self):
         self.register(ServerStart, self, self.start_server)
+        self.register(ServerOutput, self, self.server_output)
+        self.register(ServerStopped, self, self.server_stopped)
 
     async def start_server(self, event: ServerStart):
         logging.info(f"SERVER START EVENT FROM EVENT LOOP! Server name from event: {event.server_name}")
+
+        # Build the process protocol
+        self.protocol = ProcessProtocol(self.proc_closed, self.event_registry, self.loop)
+        # Connect to the server process
+        self._transport, self._protocol = await self.loop.subprocess_exec(
+            lambda: self.protocol, 
+            "python3",
+            "test.py", 
+            stdin=asyncio.subprocess.PIPE, 
+            stdout=asyncio.subprocess.PIPE, 
+            stderr=asyncio.subprocess.PIPE
+        )
+
+        logging.info("Process started... Reading some data from the process")
+
+    async def server_output(self, event: ServerOutput):
+        line = event.line
+        logging.info(f"Line from server STDOUT: {line}")
+        await self.protocol.write_process("say test")
+
+    async def server_stopped(self, event: ServerStopped):
+        logging.info(f"Server closed with reason: {event.reason}")
+
+    async def proc_closed(self, exit_code):
+        await self.event_registry.dispatch(ServerStopped(f"Process exited with code: {exit_code}"))
+        logging.info("PROC CLOSED!")
         self.loop.stop()
